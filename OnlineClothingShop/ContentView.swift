3554800d45536b48884051254be8e0dfdda3c449
lahiru-sandaruwan-dev <slahiru250@gmail.com>
2024-03-23 08:58:27 +0530

    Initial Commit

---
+++ OnlineClothingShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlineClothingShop
+//
+//  Created by Akindu Bogahawatta on 2024-03-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlineClothingShop/OnlineClothingShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineClothingShopApp.swift
+//  OnlineClothingShop
+//
+//  Created by Akindu Bogahawatta on 2024-03-23.
+//
+
+import SwiftUI
+
+@main
+struct OnlineClothingShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
