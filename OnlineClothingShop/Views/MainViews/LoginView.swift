86c9106f06524566d02927cab5f422482ce59242
lahiru-sandaruwan-dev <slahiru250@gmail.com>
2024-03-24 11:30:41 +0530

    Login page, landing page, sign in page

---
+++ OnlineClothingShop/Views/MainViews/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  OnlineClothingShop
+//
+//  Created by Akindu Bogahawatta on 2024-03-24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ OnlineClothingShop/Views/MainViews/SigninView.swift
@@ -0,0 +1,18 @@
+//
+//  SigninView.swift
+//  OnlineClothingShop
+//
+//  Created by Akindu Bogahawatta on 2024-03-24.
+//
+
+import SwiftUI
+
+struct SigninView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SigninView()
+}
